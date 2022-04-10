8c4f29922542e82b5beecefc466e2a7780038a7e
ashikjain <ashikjain@users.noreply.github.com>
2022-04-10 23:36:14 +0530

    update

---
+++ docs/script/script1.js
@@ -0,0 +1,35 @@
+
+function playbook()
+{
+    
+    div1.style.display="block";
+    div2.style.display="none";
+    div3.style.display="none";
+    image1.style.display="none";
+}
+function application(){
+    div1.style.display="none";
+    div2.style.display="block";
+    div3.style.display="none";
+    image1.style.display="none";
+}
+function overview(){
+    div1.style.display="none";
+    div2.style.display="none";
+    div3.style.display="block";
+    image1.style.display="none";
+}
+function diagram(){
+    div1.style.display="none";
+    div2.style.display="none";
+    div3.style.display="none";
+    image1.style.display="block";
+}
+function enlarge(){
+    image1.style.width="600px";
+    image1.style.height="400px";
+}
+function resize(){
+    image1.style.width="auto";
+    image1.style.height="auto";
+}
